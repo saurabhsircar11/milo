7918a05b5f1beb48086bea00cc41103ab60ad22c
Saurabh Sircar <saurabhsircar11@gmail.com>
2024-11-05 15:12:24 +0530

    feat: setup test

--- libs/blocks/locui-create/locui-create.js
+++ libs/blocks/locui-create/locui-create.js
@@ -1,5 +1,5 @@
 import { html, render } from '../../deps/htm-preact.js';
 
 export default function init(el) {
-  render(html`<h1>Milo Localization Create Flow</h1>`, el);
+  render(html`<h1>Milo Localization Create</h1>`, el);
 }
