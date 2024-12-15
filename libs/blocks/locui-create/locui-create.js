4a5e4b6d05e05b9c560a1e67237eda69ba58d227
Saurabh Sircar <saurabhsircar11@gmail.com>
2024-12-15 21:56:39 +0530

    feat: local tab test

--- libs/blocks/locui-create/locui-create.js
+++ libs/blocks/locui-create/locui-create.js
@@ -1,7 +1,16 @@
 import { createTag } from '../../utils/utils.js';
 
+function addListeners() {
+  window.addEventListener('message', (event) => {
+    // eslint-disable-next-line no-useless-return
+    if (event.origin !== 'http://localhost:5173') return;
+    window.location.search = event.data;
+  });
+}
 export default function init(el) {
   el.classList.add('container');
   const paragraph = createTag('p', null, 'hello world');
   el.append(paragraph);
+  addListeners();
+  window.postMessage('loadedWithoutParams', 'http://localhost:5173/');
 }
