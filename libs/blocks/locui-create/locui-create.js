1aac25ed690c14a087a44fae40dc9eabb623d69c
Saurabh Sircar <saurabhsircar11@gmail.com>
2024-12-15 22:43:17 +0530

    feat: added star

--- libs/blocks/locui-create/locui-create.js
+++ libs/blocks/locui-create/locui-create.js
@@ -53,5 +53,5 @@ export default function init(el) {
   const base = miloLibs || codeRoot;
   loadStyle(`${base}/blocks/milostudio-header/milostudio-header.css`);
   render(html`<${Create} />`, el);
-  window.postMessage('loadedWithParams', 'http://localhost:5173/');
+  window.postMessage('loadedWithParams', '*');
 }
